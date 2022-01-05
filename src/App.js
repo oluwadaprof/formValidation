dbb7cdf320fa7709b252c2adcdc5d166658e565a
oluwadaprof <adeekotobiloba8@gmail.com>
2022-01-05 23:44:59 +0100

    i wan sleep abeg

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FormAction from './components/FormAction';
+
+
+function App() {
+  return (
+    <div className="App">
+      <FormAction />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FormAction.jsx
@@ -0,0 +1,102 @@
+import React, {useState, useEffect} from 'react';
+
+ function FormAction  ()  {
+     const initialValues = {username: "", email: "", password: ""};
+     const [formValues, setFormValues] = useState(initialValues);
+     const [formErrors, setFormErrors] = useState({});
+     const [isSubmit , setIsSubmit] = useState(false);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormValues({...formValues, [name]: value});
+    
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        validate();
+        setIsSubmit(true);
+    }
+
+    useEffect(() => {
+        console.log(formErrors)
+        if(Object.keys(formErrors).length === 0 && isSubmit){
+            console.log(formValues)
+        }
+       
+    }, [formErrors]);
+
+    const validateUsername= () => {
+        if (!formValues.username) {
+            setFormErrors((prevState) =>({...prevState, username:"Username is required" })) 
+        } else  {
+            setFormErrors((prevState)=> ({...prevState,username:" "}))
+        }
+       
+    }
+    const validateEmail= () => {
+        const emailPattern =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (!formValues.email || !emailPattern.test(formValues.email)) {
+            setFormErrors((prevState) =>({...prevState, email:"Email is required" })) 
+        }  else  {
+            setFormErrors((prevState)=> ({...prevState,email:" "}))
+        } 
+        
+       
+    }
+    const validatePassword= () => {
+        if (!formValues.password) {
+            setFormErrors((prevState) =>({...prevState, password:"Password is required" })) 
+        } else  {
+            setFormErrors((prevState)=> ({...prevState, password:" "}))
+        }
+       
+    }
+
+    const validate = () => {
+        
+        // const regex = /^[^\$@]+@[^\$@]+\.[^\$@]{2,}$/i;
+        validateUsername()
+        validateEmail()
+        validatePassword()
+        // if (!values.username){
+        //     errors.username = "Email is required";
+        // }
+
+        // if (!values.email){
+        //     errors.email = "Email is required";
+        // }
+        // if (!values.password){
+        //     errors.password = "Password is required";
+        // }
+        // return errors;
+    };
+
+
+    return (
+        <div className='form-container'>
+            <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+            <h1 >Login Form</h1>
+            <hr className='divider' />
+            <form onSubmit={handleSubmit}>
+                <label>Username</label>
+                    <input placeholder='username'
+                     type='text' name="username" onBlur={validateUsername} value={formValues.username} onChange={handleChange}/>
+                    <p>{formErrors?.username}</p>
+
+                <label>Email</label>
+                    <input placeholder='email' 
+                    type='email' name="email" value={formValues.email} onChange={handleChange}/>
+                    <p>{formErrors?.email}</p>
+
+                <label>Password</label>
+                    <input placeholder='password' 
+                    type='password'name="password"  value={formValues.password} onChange={handleChange}/>
+                    <p>{formErrors?.password}</p>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+export default FormAction;
